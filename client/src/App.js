e20a2170126a774031b1817b4e63d1e8d78f5812
Jehanzaib <jehanzaib.dev@gmail.com>
2026-05-13 12:14:06 +0500

    Add: add public route in routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import PublicRoute from './components/routes/publicRoute.jsx';
+
 import {RegisterPage} from './pages/register.jsx';
 import LoginPage from './pages/login.jsx';
 import { HomePage } from './pages/homepage.jsx';
