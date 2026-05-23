4d1be94dd0ce33d43e17952c9456b307f0b0159f
Jehanzaib <jehanzaib.dev@gmail.com>
2026-05-23 18:19:09 +0500

    refactor: refactor for deployment

--- api/index.js
+++ api/index.js
@@ -1,33 +1,43 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import AuthRouter from './routes/authRoutes.js';
-import PostRouter from './routes/postRoutes.js';
-import UserRouter from './routes/userRoutes.js';
-import uploadRouter from './routes/uploadRoute.js';
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
 
+import AuthRouter from "./routes/authRoutes.js";
+import PostRouter from "./routes/postRoutes.js";
+import UserRouter from "./routes/userRoutes.js";
+import uploadRouter from "./routes/uploadRoute.js";
 
 dotenv.config();
 
-
-const app=express();
-
+const app = express();
 
 app.use(express.json());
-app.use(cors());
-app.use("/images",
-	express.static("public/images"));
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use(
+  "/images",
+  express.static("public/images")
+);
+
 app.use("/api/v1/upload", uploadRouter);
+app.use("/api/v1/auth", AuthRouter);
+app.use("/api/v1/posts", PostRouter);
+app.use("/api/v1/users", UserRouter);
 
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
-app.use('/api/v1/auth', AuthRouter);
-app.use('/api/v1/posts', PostRouter);
-app.use('/api/v1/users', UserRouter);
+const PORT = process.env.PORT || 8800;
 
-
-mongoose.connect(process.env.MONGODB_URI).then(()=>console.log("mongoDB connected")).catch(err=>console.log(err));
-
-app.listen(8800, ()=>{
-	console.log("server running on port 8800");
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
