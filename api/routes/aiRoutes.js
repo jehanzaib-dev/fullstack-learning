61da0729851a47887b4ccdacb316ec0096d793d8
Jehanzaib <jehanzaib.dev@gmail.com>
2026-06-22 22:35:10 +0500

    refactor: rename AI route from /generate-post to /generate-text

--- api/routes/aiRoutes.js
+++ api/routes/aiRoutes.js
@@ -3,6 +3,6 @@ import { generatePost } from "../controllers/aiController.js";
 
 const AIRouter = express.Router();
 
-AIRouter.route("/generate-post").post(generatePost);
+AIRouter.route("/generate-text").post(generatePost);
 
 export default AIRouter;
