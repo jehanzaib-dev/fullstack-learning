b0ca0603e11ead525bb2973c6fb8f45a88229211
Jehanzaib <jehanzaib.dev@gmail.com>
2026-06-22 12:06:21 +0500

    backup before AI Integration

--- api/routes/postRoutes.js
+++ api/routes/postRoutes.js
@@ -1,10 +1,9 @@
 import Router from 'express';
-import {CreatePost, deletePost, getAllPosts, getUserPosts, likePost, editPost, addComment, getTimelinePosts} from '../controllers/postController.js';
+import {CreatePost, deletePost, getUserPosts, likePost, editPost, addComment, getTimelinePosts} from '../controllers/postController.js';
 
 const postRouter=Router();
 
 postRouter.route('/').post(CreatePost);
-postRouter.route('/').get(getAllPosts);
 postRouter.route('/profile/:username').get(getUserPosts);
 postRouter.route('/:id/like').put(likePost);
 postRouter.route('/:id').delete(deletePost);
